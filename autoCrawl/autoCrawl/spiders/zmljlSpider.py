e3adf06a22e7352074fbf5affeb52c64737daa5b
wpy <565719165@qq.com>
2022-05-20 14:41:17 +0800

    爬起政民零距离第一页

---
+++ autoCrawl/autoCrawl/spiders/zmljlSpider.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+class ZmljlSpider(scrapy.Spider):
+    name = "zmljl"
+    start_urls = [
+        'http://jyhpt.tj.gov.cn/zmljl/list?resultDeptType=1&pageIndex=1&pageSize=10&_=1653013889574'
+    ]
+
+    def parse(self,response):
+        sites = json.loads(response.body)
+        json.dump(sites,fp=open('E:/auto-tjcrawl-data/1.json','w'),ensure_ascii=False)
+        # if next_page_url is not None:
+        #     yield scrapy.Request(response.urljosnn)
