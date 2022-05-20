d7215198e7f22bb26b5b7e57222c742dae8186e2
wpy <565719165@qq.com>
2022-05-20 14:44:35 +0800

    在wpy-dev下提交增加一个注释

--- autoCrawl/autoCrawl/spiders/zmljlSpider.py
+++ autoCrawl/autoCrawl/spiders/zmljlSpider.py
@@ -10,6 +10,6 @@ class ZmljlSpider(scrapy.Spider):
 
     def parse(self,response):
         sites = json.loads(response.body)
-        json.dump(sites,fp=open('E:/auto-tjcrawl-data/1.json','w'),ensure_ascii=False)
+        # json.dump(sites,fp=open('E:/auto-tjcrawl-data/1.json','w'),ensure_ascii=False)
         # if next_page_url is not None:
         #     yield scrapy.Request(response.urljosnn)
